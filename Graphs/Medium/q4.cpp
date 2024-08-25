c22f3be158047f38691b2e54ea847bee5ea65313
Athul Krishna <ak021coder@gmail.com>
2024-08-25 10:26:51 +0530

    Graph

---
+++ Graphs/Medium/q4.cpp
@@ -0,0 +1,71 @@
+// 0/1 Matrix
+
+// Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+// The distance between two adjacent cells is 1.
+
+// Example 1:
+
+// Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: [[0,0,0],[0,1,0],[0,0,0]]
+
+
+// Example 2:
+// Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+// Output: [[0,0,0],[0,1,0],[1,2,1]]
+ 
+
+// Constraints:
+// m == mat.length
+// n == mat[i].length
+// 1 <= m, n <= 104
+// 1 <= m * n <= 104
+// mat[i][j] is either 0 or 1.
+// There is at least one 0 in mat.
+
+// I tried with 1 as multisource , but correct way was 0 as multisource
+
+// class Solution {
+// public:
+//     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+//         int n = mat.size();
+//         int m = mat[0].size();
+
+//         vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
+//         queue<pair<int, int>> q;
+        
+//         // Step 1: Initialize the queue with all the cells containing 0 and set their distance to 0.
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (mat[i][j] == 0) {
+//                     ans[i][j] = 0;
+//                     q.push({i, j});
+//                 }
+//             }
+//         }
+
+//         int drow[] = {-1, 0, +1, 0};
+//         int dcol[] = {0, +1, 0, -1};
+
+//         // Step 2: BFS from all 0 cells simultaneously.
+//         while (!q.empty()) {
+//             int r = q.front().first;
+//             int c = q.front().second;
+//             q.pop();
+
+//             for (int i = 0; i < 4; i++) {
+//                 int nrow = r + drow[i];
+//                 int ncol = c + dcol[i];
+//                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
+//                     if (ans[nrow][ncol] > ans[r][c] + 1) {
+//                         ans[nrow][ncol] = ans[r][c] + 1;
+//                         q.push({nrow, ncol});
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
